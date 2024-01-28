b53cabd252ea29bde1f1a0d8e466cb48d7574c16
jongju-Lee <jeje7@naver.com>
2024-01-29 01:41:09 +0900

    Sub Page Complete

--- Architecture Agency/custom.js
+++ Architecture Agency/custom.js
@@ -22,6 +22,17 @@ $(function () {
     }
   });
   // ##### Slick.js - Section : Project #####
+  $(".project-photo").slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    cssEase: "linear",
+  });
 
   // ##### Slick.js - Section : Plan #####
   $(".history-slider").slick({
@@ -31,30 +42,5 @@ $(function () {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
   });
 });
