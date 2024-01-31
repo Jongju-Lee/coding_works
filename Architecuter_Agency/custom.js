c57091b62345ca05761a944443e2f6fbb7dfd93d
jongju-Lee <jeje7@naver.com>
2024-01-31 21:11:32 +0900

    Agency 2 All Complete

--- Architecuter_Agency/custom.js
+++ Architecuter_Agency/custom.js
@@ -45,3 +45,21 @@ $(".project-photo").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
 });
+/* Header - Scroll Event */
+$(window).scroll(function () {
+  if (window.scrollY >= 50) {
+    // 스크롤 다운이 50 이상이면
+    $("header").addClass("active");
+  } else if (window.screenY <= 50) {
+    // 스크롤 다운이 50 이하이면
+    $("header").removeClass("active");
+  }
+});
+/* Header - Trigger */
+$(".trigger").click(function () {
+  $(this).toggleClass("active");
+  $(".gnb").toggleClass("active");
+});
+$(".gnb .menu a").click(function () {
+  $(".trigger, .gnb").removeClass("active");
+});
